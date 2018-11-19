588cff48c8da6b047a646b1a1bd67a51711c45ec
[sterrevangeest] <[sterrevangeest@hotmail.nl]>
2018-11-19 23:00:45 +0100

    re-structured .js file after Justus' talk

--- data/index.js
+++ data/index.js
@@ -1,6 +1,22 @@
-// console.log("testtesttest");
 const OBA = require("oba-api");
 var fs = require("file-system");
+
+// const rp = require("request-promise");
+// const $ = require("cheerio");
+// const url = "https://en.wikipedia.org/wiki/Anchee_Min";
+//
+// rp(url)
+//   .then(function(html) {
+//     //success!
+//     const wikiUrls = [];
+//     for (let i = 0; i < 45; i++) {
+//       wikiUrls.push($(".firstHeading", html)[i].attribs.href);
+//     }
+//     console.log(wikiUrls);
+//   })
+//   .catch(function(err) {
+//     //handle error
+//   });
 
 require("dotenv").config();
 
@@ -18,89 +34,141 @@ const client = new OBA({
 
 // Client returns a promise which resolves the APIs output in JSON
 
-var allData = [];
-
+// for (var i = 0; i < 56; i++) {
 client
   .get(
     // get all books
     "search",
     {
-      q: "format:book",
+      q: "genre:biografie",
       librarian: true,
-      refine: true
-    },
-    "title"
+      refine: true,
+      page: 1
+    }
   )
   .then(response => JSON.parse(response).aquabrowser)
   .then(response => {
-    var selectedRctx = [];
-    var selectedYears = [];
-
-    var years = getYears(selectedYears);
-
-    selectedYears.forEach(function(years) {
-      client
-        .get("search", {
-          q: years,
-          librarian: true,
-          refine: true
-        })
-        .then(response => JSON.parse(response).aquabrowser)
-        .then(response => {
-          var rctx = response.meta.rctx;
-          selectedRctx.push(rctx);
-
-          if (selectedRctx.length == 11) {
-            selectedRctx.forEach(function(selectedRctx) {
-              client
-                .get("refine", {
-                  rctx: selectedRctx,
-                  count: 100
-                })
-                .then(response => JSON.parse(response).aquabrowser)
-                .then(response => {
-                  var genreFacet = getGenreFacet(response);
-                });
-            });
-          }
-        });
-    });
+    var data = getData(response);
   })
+  .catch(err => console.error(err));
+// Something went wrong in the request to the API
+//}
 
-  .catch(err => console.log(err)); // Something went wrong in the request to the API
+function getData(response) {
+  var result = response.results.result;
 
-function getYears(selectedYears) {
-  var period = 50;
+  var author = getAuthor(result);
+  var title = getTitle(result);
+  var summary = getSummary(result);
+  var publicationYear = getPublicationYear(result);
+  var gender = getGender(summary);
+  var authorUrl = makeAuthorUrl(author);
 
-  for (var i = 0; i <= period; i = i + 5) {
-    var year = "year:" + (1965 + i);
-    selectedYears.push(year);
-  }
-}
+  var dataStore = result.map((d, i) => {
+    return {
+      author: author[i],
+      title: title[i],
+      summary: summary[i],
+      publicationYear: publicationYear[i],
+      gender: gender[i],
+      authorUrl: authorUrl[i]
+    };
+    // console.log(dataStore);
+    console.log("gelukt");
+  });
 
-function getGenreFacet(data) {
-  var languageId = data.meta["original-query"];
-  var year = languageId.slice(6, 10);
-  var facets = data.facets.facet;
-
-  facets.forEach(function(facets) {
-    var facetId = facets.id;
-    if (facetId === "Genre") {
-      var values = facets.value;
-      values.forEach(function(values) {
-        var count = values.count;
-        var id = values.id;
-        allData.push({
-          year: year,
-          genre: id,
-          count: count
-        });
-        var allDataJson = JSON.stringify(allData);
-        fs.writeFileSync("data.json", allDataJson, err => {
-          if (err) throw err;
-          console.log("All data written to data.json");
-        });
-      });
-    }
+  var allDataJson = JSON.stringify(dataStore);
+  fs.appendFileSync("data/data.json", allDataJson, err => {
+    if (err) throw err;
   });
 }
+
+function getAuthor(result) {
+  var authorsAndTitles = result.map(result => result.titles.title["$t"]);
+  var authors = authorsAndTitles.map(authorsAndTitles => {
+    if (authorsAndTitles == undefined) {
+      return "no title";
+    } else {
+      return authorsAndTitles.split(" / ").pop();
+    }
+  });
+
+  return authors;
+}
+
+function getTitle(result) {
+  var authorsAndTitles = result.map(result => result.titles.title["$t"]);
+  var title = authorsAndTitles.map(authorsAndTitles => {
+    if (authorsAndTitles == undefined) {
+      return "no au";
+    } else {
+      return authorsAndTitles.split(" / ").pop();
+    }
+  });
+  return title;
+}
+
+function getSummary(result) {
+  var summaries = result.map(result => result.summaries);
+  var summaryTexts = summaries.map(summary => {
+    if (summary != undefined) {
+      if (true) {
+      }
+      return summary.summary["$t"];
+    } else if (summary == undefined) {
+      return "GEEN SAMENVATTING";
+    }
+  });
+  return summaryTexts;
+}
+
+function getPublicationYear(result) {
+  var publicationYear = result.map(result => {
+    if (publicationYear === undefined) {
+      return "noPublicationYear";
+    } else {
+      parseInt(result.publication.publishers.publisher.year.replace(/\D/g, ""));
+    }
+  });
+  return publicationYear;
+}
+
+function getGender(summary) {
+  var gender = summary.map(summary => {
+    if (
+      summary.includes("vrouw") ||
+      summary.includes("zij ") ||
+      summary.includes("haar")
+    ) {
+      return "female";
+    } else if (
+      summary.includes("man") ||
+      summary.includes("zijn") ||
+      summary.includes("hem")
+    ) {
+      return "male";
+    } else {
+      return "unknown";
+    }
+  });
+  return gender;
+}
+
+function makeAuthorUrl(author) {
+  console.log(author);
+  var urlAuthor = author.map(author => {
+    if (
+      author.charCodeAt(0) === author.toUpperCase().charCodeAt(0) &&
+      author.length < 35
+    ) {
+      var allAuthors = author;
+      var urlAuthor = allAuthors.replace(/ /g, "_");
+    } else {
+      var urlAuthor = "noURL";
+    }
+    return urlAuthor;
+  });
+  return urlAuthor;
+}
+
+// TODO: use authorURL for Wikipedia API request, to check gender of author
